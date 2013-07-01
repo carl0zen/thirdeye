32d0ca7549def873dbc50b3353cf87281ab0a720
Carlos <carlosepp@gmail.com>
2013-07-01 13:23:35 -0500

    Angular.js implementation incomplete

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,16 @@
-(function() {
-
-	
 
 
-}).call(this);
+angular.module('myApp',[]).
+	config(function($routeProvider){
+		$routeProvider.
+			when('/home', {templateUrl: '/views/main.html', controller: 'HomeCtrl'}).
+			when('/view1', {templateUrl: '/views/view1.html', controller: 'HomeCtrl'}).
+			otherwise({redirectTo:'/home'});
+	});
+
+
+function HomeCtrl($scope){
+
+
+	$scope.total = "Learn angular";
+}

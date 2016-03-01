84dc216988a029182d8b6beaba7083ebf1064018
Carlos Perezpriego <carlosepp@gmail.com>
2016-03-01 01:29:38 -0600

    Create basic folder structure

---
+++ src/entry.js
@@ -0,0 +1,21 @@
+// include styles into bundle.js
+// so you don't need it to add them in index.html
+import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./views/home";
+
+// import some modules from other files
+
+const App = (props) => {
+  return(
+    <Home />
+  )
+}
+
+const container = document.getElementById('container');
+
+ReactDOM.render(
+  <App />,
+  container
+);
---
+++ src/views/home/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./styles.scss";
+
+const Home = (props) => <strong className={ styles.root } > HOMES </strong>
+
+export default Home
